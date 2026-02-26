82873376b631d309e4d9a0916933edc5de98e231
codebykenth <kenthosila@gmail.com>
2026-02-27 01:15:31 +0800

    Update scheduled time of account balance fetch

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,6 @@
 
 if (app()->environment('production')) {
     Schedule::command('trades:fetch-pnl')->hourly();
-    Schedule::command('account:fetch-balance')->dailyAt('1:00')->timezone('Asia/Manila');
+    Schedule::command('account:fetch-balance')->dailyAt('1:30')->timezone('Asia/Manila');
     Schedule::command('generate:daily-news')->dailyAt('08:00')->timezone('Asia/Manila');
 }
