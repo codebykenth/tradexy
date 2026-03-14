f0da59cac83ca93212e67a201d636283d444c7d9
codebykenth <kenthosila@gmail.com>
2026-03-14 23:07:37 +0800

    Update time in schedules

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,13 @@
 <?php
 
-if (app()->environment(['production', 'staging'])) {
-    Schedule::command('trades:fetch-pnl')->hourly();
-    Schedule::command('trades:fetch-pnl --demo')->hourlyAt(30);
-    Schedule::command('account:fetch-balance')->dailyAt('2:00')->timezone('Asia/Manila');
-    Schedule::command('account:fetch-balance --demo')->dailyAt('2:05')->timezone('Asia/Manila');
+if (app()->environment(['production'])) {
+    // Fetch trades every 15 mins for faster updates
+    Schedule::command('trades:fetch-pnl')->everyFifteenMinutes()->withoutOverlapping();
+    Schedule::command('trades:fetch-pnl --demo')->everyFifteenMinutes()->withoutOverlapping();
+
+    // Fetch account balance daily at 2:00 AM (local time)
+    Schedule::command('account:fetch-balance')->dailyAt('02:00')->timezone('Asia/Manila')->withoutOverlapping();
+    Schedule::command('account:fetch-balance --demo')->dailyAt('02:05')->timezone('Asia/Manila')->withoutOverlapping();
 }
 
 if (app()->environment('production')) {
